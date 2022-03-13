e76d9cd664a039e67ddcc930cabce683a55f5efa
guyllaume demers <Guyllaume@technodramatists.com>
2022-03-13 15:17:39 -0400

    pushing random pattern

--- SDL2-BulletHell/SDL2-BulletHell/DanmakuImp_1.cpp
+++ SDL2-BulletHell/SDL2-BulletHell/DanmakuImp_1.cpp
@@ -14,10 +14,10 @@ DanmakuImp_1::~DanmakuImp_1() {}
 void DanmakuImp_1::create(const Vector2d& location)
 {
 	subpatterns = {
-		DBG_NEW Ring(location, 360, 0.100f, 0, 0, 000, TextureManager::getTexture("CP1")),
-		DBG_NEW Ring(location, 120, 0.050f, 0, 0, 000, TextureManager::getTexture("DP1")),
-		DBG_NEW Ring(location, 120, 0.035f, 0, 0, 120, TextureManager::getTexture("DP1")),
-		DBG_NEW Ring(location, 120, 0.015f, 0, 0, 045, TextureManager::getTexture("CPG1"))
+		DBG_NEW Ring(location, 045, 0.100f, 0, 0, 90, TextureManager::getTexture("CP1")),
+		DBG_NEW Ring(location, 045, 0.050f, 0, 0, 45, TextureManager::getTexture("DP1")),
+		DBG_NEW Ring(location, 045, 0.035f, 0, 0, 120, TextureManager::getTexture("DP1")),
+		DBG_NEW Ring(location, 045, 0.015f, 0, 0, 145, TextureManager::getTexture("CPG1"))
 	};
 }
 
